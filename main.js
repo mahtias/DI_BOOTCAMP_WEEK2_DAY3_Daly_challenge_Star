b8fe08cc47b5b2c6128f1985b66a7445f1359ad3
mahtias <kadjomathias1994@gmail.com>
2023-01-10 13:14:06 +0000

    frist depot

---
+++ main.js
@@ -0,0 +1,13 @@
+// premiere boucle
+for (let i = 0; i < 6; i++) {
+  console.log(" ");
+}
+
+
+// boucle imbriquer
+for (let i = 0; i <= 6; i++) {
+  console.log("- First level loop");
+  for (let j = 0; j <= 6; j++) {
+    console.log("-- Second level loop");
+  }
+}
